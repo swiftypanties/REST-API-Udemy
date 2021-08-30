d9b002f33db97e4e62f8603e4e03ba9551f8655d
swiftypanties <sonyon.winx@gmail.com>
2021-08-30 21:30:14 +0300

    added put endpoint and improved code

--- code/app.py
+++ code/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -13,6 +13,13 @@ jwt = JWT(app,authenticate, identity)   #/auth
 items = []
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank!"
+    )
+
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
@@ -21,7 +28,8 @@ class Item(Resource):
     def post(self, name):
         if next(filter(lambda x: x['name'] == name, items), None) is not None:
             return {'message': "An item with name '{}' already exists".format(name)}, 400
-        data = request.get_json()
+
+        data = Item.parser.parse_args()    
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
@@ -32,6 +40,18 @@ class Item(Resource):
                 items.remove(item)
                 return {'message': "The'{}' is deleted".format(name)}
         return {'message': "item named '{}' not found in the list".format(name)}
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+
+
 
 class ItemList(Resource):
     def get(self):
