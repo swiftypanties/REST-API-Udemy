fc0e978f6b8081b02be0b61a545265513442a2a1
swiftypanties <sonyon.winx@gmail.com>
2021-09-20 18:39:56 +0300

    now support deleting specific item from database

--- code/item.py
+++ code/item.py
@@ -47,11 +47,16 @@ class Item(Resource):
         return item, 201
 
     def delete (self, name):
-        for item in items:
-            if item['name'] == name:
-                items.remove(item)
-                return {'message': "The'{}' is deleted".format(name)}
-        return {'message': "item named '{}' not found in the list".format(name)}
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "DELETE FROM items WHERE name=?"
+        cursor.execute(query, (name,))
+
+        connection.commit()
+        connection.close()
+        return {'message': 'Item deleted'}
+
 
     def put(self, name):
         data = Item.parser.parse_args()
